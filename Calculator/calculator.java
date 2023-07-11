d40bbf7a2f02de854d5fca5634fe469fd0a70b0e
tk0515 <thaliagkennedy@gmail.com>
2023-07-11 15:24:53 -0500

    Created main java file and some blanket print statements

---
+++ Calculator/calculator.java
@@ -0,0 +1,16 @@
+/**
+ * File: calculator.java
+ * 
+ * @author: Thalia Kennedy, thaliagkennedy@gmail.com
+ * 
+ * Program will calculate how much pizza should be ordered and what type(s).
+ */
+
+ //import java.util.Scanner;
+
+ public class calculator{
+    public static void main(String[] args){
+        System.out.println("Welcome!");
+        System.out.println("Who all will be eating?");
+    }
+ }
